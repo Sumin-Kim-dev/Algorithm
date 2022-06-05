dc0ca06f75ca5c05c5e6c6701016c016759327a4
Sumin-Kim-dev <sumin.kim.develop@gmail.com>
2022-06-05 15:06:17 +0900

    20220604

---
+++ boj2580.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class boj2580 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int sudoku[][] = new int[9][9];
+		for (int i = 0; i < 9; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 9; j++)
+				sudoku[i][j] = Integer.parseInt(st.nextToken());
+		}
+	}
+}
