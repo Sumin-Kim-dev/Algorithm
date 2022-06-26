8c1040d46913e5ef4e0a3d658b3559b09ce8c913
Sumin-Kim-dev <sumin.kim.develop@gmail.com>
2022-06-27 01:09:27 +0900

    2022-06-26

---
+++ src/boj/BOJ17386.java
@@ -0,0 +1,59 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ17386 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		long[][] points = new long[4][2];
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
+		for (int i = 0; i < 4; i++) {
+			points[i / 2][i % 2] = Integer.parseInt(st1.nextToken());
+			points[i / 2 + 2][i % 2] = Integer.parseInt(st2.nextToken());
+		}
+		long[] calc = calc(points);
+		int ans = 0;
+		if ((double) calc[0] * calc[1] < 0 && (double) calc[2] * calc[3] < 0)
+			ans = 1;
+		if (calc[2] == 0 && (points[0][0] - points[2][0]) * (points[0][0] - points[3][0]) <= 0
+				&& (points[0][1] - points[2][1]) * (points[0][1] - points[3][1]) <= 0)
+			ans = 1;
+		if (calc[3] == 0 && (points[1][0] - points[2][0]) * (points[1][0] - points[3][0]) <= 0
+				&& (points[1][1] - points[2][1]) * (points[1][1] - points[3][1]) <= 0)
+			ans = 1;
+		if (calc[0] == 0 && (points[2][0] - points[0][0]) * (points[2][0] - points[1][0]) <= 0
+				&& (points[2][1] - points[0][1]) * (points[2][1] - points[1][1]) <= 0)
+			ans = 1;
+		if (calc[1] == 0 && (points[3][0] - points[0][0]) * (points[3][0] - points[1][0]) <= 0
+				&& (points[3][1] - points[0][1]) * (points[3][1] - points[1][1]) <= 0)
+			ans = 1;
+		bw.write(ans + "");
+		bw.close();
+	}
+
+	static long[] calc(long[][] points) {
+		long[] calc = new long[4];
+		// point2°¡ point0, 1 ÀÕ´Â ¼±»ó
+		calc[0] = (points[1][1] - points[0][1]) * (points[2][0] - points[0][0])
+				+ (points[0][0] - points[1][0]) * (points[2][1] - points[0][1]);
+		// point3ÀÌ point0, 1 ÀÕ´Â ¼±»ó
+		calc[1] = (points[1][1] - points[0][1]) * (points[3][0] - points[0][0])
+				+ (points[0][0] - points[1][0]) * (points[3][1] - points[0][1]);
+		// point0ÀÌ point2, 3 ÀÕ´Â ¼±»ó
+		calc[2] = (points[3][1] - points[2][1]) * (points[0][0] - points[2][0])
+				+ (points[2][0] - points[3][0]) * (points[0][1] - points[2][1]);
+		// point1ÀÌ point2, 3 ÀÕ´Â ¼±»ó
+		calc[3] = (points[3][1] - points[2][1]) * (points[1][0] - points[2][0])
+				+ (points[2][0] - points[3][0]) * (points[1][1] - points[2][1]);
+		return calc;
+	}
+}
---
+++ src/boj/BOJ1753.java
@@ -0,0 +1,98 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ1753 {
+
+	final static int INF = 200001;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int v = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(br.readLine());
+		LinkedList<Edge>[] edge = new LinkedList[v];
+		for (int i = 0; i < v; i++) {
+			edge[i] = new LinkedList<>();
+		}
+		for (int i = 0; i < e; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken()) - 1;
+			int end = Integer.parseInt(st.nextToken()) - 1;
+			edge[start].add(new Edge(end, Integer.parseInt(st.nextToken())));
+		}
+		int[] ans = dijkstra(edge, k - 1);
+		for (int i = 0; i < v; i++) {
+			if (ans[i] != INF)
+				sb.append(ans[i]);
+			if (ans[i] == INF)
+				sb.append("INF");
+			sb.append('\n');
+		}
+		bw.write(sb.toString());
+		bw.close();
+	}
+
+	static int[] dijkstra(LinkedList<Edge>[] edge, int k) {
+		int[] dist = new int[edge.length];
+		PriorityQueue<Vertex> pq = new PriorityQueue<>();
+		Arrays.fill(dist, INF);
+		pq.offer(new Vertex(k, 0));
+		dist[k] = 0;
+		while (!pq.isEmpty()) {
+			Vertex curr = pq.poll();
+			// 이미 방문한 점은 스킵
+			if (dist[curr.node] < curr.minDist)
+				continue;
+			// 이웃한 점 확인
+			Iterator<Edge> iter = edge[curr.node].iterator();
+			while (iter.hasNext()) {
+				Edge e = iter.next();
+				// 새 경로가 더 가깝다면 갱신
+				if (dist[curr.node] + e.dist < dist[e.end]) {
+					dist[e.end] = dist[curr.node] + e.dist;
+					pq.offer(new Vertex(e.end, dist[e.end]));
+				}
+			}
+		}
+		return dist;
+	}
+}
+
+class Vertex implements Comparable<Vertex> {
+
+	final static int INF = 200001;
+
+	int node, minDist;
+
+	Vertex(int node, int minDist) {
+		this.node = node;
+		this.minDist = minDist;
+	}
+
+	public int compareTo(Vertex v) {
+		return this.minDist - v.minDist;
+	}
+}
+
+class Edge {
+	int end, dist;
+
+	Edge(int end, int dist) {
+		this.end = end;
+		this.dist = dist;
+	}
+}
