f3407df0bd22ba2835ffe0014ea6f674ae096c62
Sumin-Kim-dev <sumin.kim.develop@gmail.com>
2022-06-27 06:14:37 +0900

    edit gitignore

---
+++ src/BOJ1504.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ1504 {
+	
+	class Node implements Comparable<Node>{
+		int node, minDist;
+		Node(int node, int minDist) {
+			this.node = node;
+			this.minDist = minDist;
+		}
+		public int compareTo(Node n) {
+			return this.minDist - n.minDist;
+		}
+	}
+	
+	final int INF = Integer.MAX_VALUE;
+	int n, e, v1, v2;
+	int[][] adj;
+	
+	private void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		e = Integer.parseInt(st.nextToken());
+		adj = new int[n][n];
+		for(int i = 0; i < e; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
+			adj[a][b] = adj[b][a] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		v1 = Integer.parseInt(st.nextToken()) - 1;
+		v2 = Integer.parseInt(st.nextToken()) - 1;
+	}
+	
+	private int[] Dijkstra(int start) {
+		int[] dist = new int[n];
+		Arrays.fill(dist, INF);
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(start, 0));
+		dist[start] = 0;
+		while(!pq.isEmpty()) {
+			Node curr = pq.poll();
+			int currNode = curr.node;
+			int currDist = curr.minDist;
+			if(dist[currNode] < currDist)
+				continue;
+			for(int i = 0; i < n; i++) {
+				if(adj[currNode][i] == 0)
+					continue;
+				if(dist[i] > currDist + adj[currNode][i]) {
+					dist[i] = currDist + adj[currNode][i];
+					pq.add(new Node(i, dist[i]));
+				}
+			}
+		}
+		return dist;
+	}
+	
+	private void solution() throws IOException {
+		input();
+		int dist = INF;
+		int[] dist1 = Dijkstra(v1), dist2 = Dijkstra(v2);
+		// 1 -> v1 -> v2 -> n
+		if(dist1[0] != INF && dist1[v2] != INF && dist2[n-1] != INF)
+			dist = dist1[0] + dist1[v2] + dist2[n-1];
+		// 1 -> v2 -> v1 -> n
+		if(dist2[0] != INF && dist2[v1] != INF && dist1[n-1] != INF)
+			if(dist > dist2[0] + dist2[v1] + dist1[n-1])
+				dist = dist2[0] + dist2[v1] + dist1[n-1];
+		if(dist == INF) dist = -1;
+		print(dist);
+	}
+	
+	private void print(int ans) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(ans+"");
+		bw.close();
+	}
+	
+	public static void main(String[] args) throws IOException {
+		new BOJ1504().solution();
+	}
+
+}
---
+++ src/BOJ17386.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ17386 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		long[][] points = new long[4][2];
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
+		for (int i = 0; i < 4; i++) {
+			points[i / 2][i % 2] = Integer.parseInt(st1.nextToken());
+			points[i / 2 + 2][i % 2] = Integer.parseInt(st2.nextToken());
+		}
+		long[] calc = calc(points);
+		int ans = 0;
+		if ((double) calc[0] * calc[1] < 0 && (double) calc[2] * calc[3] < 0)
+			ans = 1;
+		bw.write(ans + "");
+		bw.close();
+	}
+
+	static long[] calc(long[][] points) {
+		long[] calc = new long[4];
+		// point2는 point0, 1 잇는 직선과 위치 비교
+		calc[0] = (points[1][1] - points[0][1]) * (points[2][0] - points[0][0])
+				+ (points[0][0] - points[1][0]) * (points[2][1] - points[0][1]);
+		// point3은 point0, 1 잇는 직선과 위치 비교
+		calc[1] = (points[1][1] - points[0][1]) * (points[3][0] - points[0][0])
+				+ (points[0][0] - points[1][0]) * (points[3][1] - points[0][1]);
+		// point0은 point1, 2 잇는 직선과 위치 비교
+		calc[2] = (points[3][1] - points[2][1]) * (points[0][0] - points[2][0])
+				+ (points[2][0] - points[3][0]) * (points[0][1] - points[2][1]);
+		// point1은 point1, 2 잇는 직선과 위치 비교
+		calc[3] = (points[3][1] - points[2][1]) * (points[1][0] - points[2][0])
+				+ (points[2][0] - points[3][0]) * (points[1][1] - points[2][1]);
+		return calc;
+	}
+}
