7d8988b3fdd402bc7b187a7f55f7d9ef59813290
Sumin-Kim-dev <sumin.kim.develop@gmail.com>
2022-07-09 22:00:42 +0900

    트리의 지름 : Gold 2

---
+++ src/BOJ1167.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ1167 {
+    int n;
+    ArrayList<Node>[] adj;
+
+    class Node {
+        int node, dist;
+
+        Node(int node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+    }
+
+    void makeGraph() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        adj = new ArrayList[n];
+        for (int i = 0; i < n; i++)
+            adj[i] = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            while (true) {
+                int end = Integer.parseInt(st.nextToken()) - 1;
+                if(end == -2) break;
+                int weight = Integer.parseInt(st.nextToken());
+                adj[start].add(new Node(end, weight));
+            }
+        }
+    }
+
+    Node maxDist(int index) {
+        int[] dist = new int[n];
+        Arrays.fill(dist, -1);
+        dist[index] = 0;
+        dfs(index, dist);
+        int maxIndex = 0;
+        int maxDist = 0;
+        for (int i = 0; i < n; i++) {
+            if (dist[i] > maxDist) {
+                maxDist = dist[i];
+                maxIndex = i;
+            }
+        }
+        return new Node(maxIndex, maxDist);
+    }
+
+    void dfs(int index, int[] dist) {
+        int size = adj[index].size();
+        for (int i = 0; i < size; i++) {
+            Node next = adj[index].get(i);
+            if (dist[next.node] == -1) {
+                dist[next.node] = dist[index] + next.dist;
+                dfs(next.node, dist);
+            }
+        }
+    }
+
+    void solution() throws IOException {
+        makeGraph();
+        int a = maxDist(0).node;
+        int radius = maxDist(a).dist;
+        System.out.println(radius);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new BOJ1167().solution();
+    }
+}
