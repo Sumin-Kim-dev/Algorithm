c073baa9de9af554ddb98dc20723f3cc40553137
Sumin-Kim-dev <sumin.kim.develop@gmail.com>
2022-07-10 23:55:13 +0900

    BOJ1520 : import문 정리

--- src/BOJ1520.java
+++ src/BOJ1520.java
@@ -3,43 +3,43 @@ import java.util.StringTokenizer;
 
 public class BOJ1520 {
 
-	static int m, n;
-	static int[][] map, path;
+    static int m, n;
+    static int[][] map, path;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		m = Integer.parseInt(st.nextToken());
-		n = Integer.parseInt(st.nextToken());
-		map = new int[m][n];
-		path = new int[m][n];
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < n; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				path[i][j] = -1; // 아직 안 구한 값은 -1로 초기화
-			}
-		}
-		bw.write(path(m - 1, n - 1) + "");
-		bw.close();
-	}
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        map = new int[m][n];
+        path = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                path[i][j] = -1; // 아직 안 구한 값은 -1로 초기화
+            }
+        }
+        bw.write(path(m - 1, n - 1) + "");
+        bw.close();
+    }
 
-	static int path(int i, int j) {
-		if (i == 0 && j == 0)
-			return 1;
-		if (path[i][j] != -1)
-			return path[i][j];
-		path[i][j] = 0;
-		if (i > 0 && map[i - 1][j] > map[i][j])
-			path[i][j] += path(i - 1, j);
-		if (j > 0 && map[i][j - 1] > map[i][j])
-			path[i][j] += path(i, j - 1);
-		if (i < m - 1 && map[i + 1][j] > map[i][j])
-			path[i][j] += path(i + 1, j);
-		if (j < n - 1 && map[i][j + 1] > map[i][j])
-			path[i][j] += path(i, j + 1);
-		return path[i][j];
-	}
+    static int path(int i, int j) {
+        if (i == 0 && j == 0)
+            return 1;
+        if (path[i][j] != -1)
+            return path[i][j];
+        path[i][j] = 0;
+        if (i > 0 && map[i - 1][j] > map[i][j])
+            path[i][j] += path(i - 1, j);
+        if (j > 0 && map[i][j - 1] > map[i][j])
+            path[i][j] += path(i, j - 1);
+        if (i < m - 1 && map[i + 1][j] > map[i][j])
+            path[i][j] += path(i + 1, j);
+        if (j < n - 1 && map[i][j + 1] > map[i][j])
+            path[i][j] += path(i, j + 1);
+        return path[i][j];
+    }
 }
