28e01ebedb5d901c16054d962c796b9d4b668b39
Sumin-Kim-dev <sumin.kim.develop@gmail.com>
2022-07-25 00:30:15 +0900

    카드 정렬하기 : Gold 4

---
+++ src/BOJ1715.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BOJ1715 {
+    int n;
+    PriorityQueue<Integer> cards;
+
+    void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        cards = new PriorityQueue<>();
+        for(int i = 0; i < n; i++)
+            cards.add(Integer.parseInt(br.readLine()));
+    }
+
+    long count() {
+        long count = 0;
+        while(cards.size() > 1) {
+            int first = cards.poll();
+            int second = cards.poll();
+            cards.add(first + second);
+            count += first + second;
+        }
+        return count;
+    }
+
+    void solution() throws IOException {
+        input();
+        System.out.println(count());
+    }
+
+    public static void main(String[] args) throws IOException {
+        new BOJ1715().solution();
+    }
+}
