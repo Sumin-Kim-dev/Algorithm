8a7d7d7dd8019f30d2b96e59e919d30c2a7cd114
Sumin-Kim-dev <sumin.kim.develop@gmail.com>
2022-08-07 11:03:03 +0900

    킹, 퀸, 룩, 비숍, 나이트, 폰 : Bronze 5

--- src/BOJ3003.java
+++ src/BOJ3003.java
@@ -5,10 +5,11 @@ import java.util.StringTokenizer;
 
 public class BOJ3003 {
     static final int[] chess = {1, 1, 2, 2, 2, 8};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i : chess) {
+        for (int i : chess) {
             System.out.print(i - Integer.parseInt(st.nextToken()));
             System.out.print(' ');
         }
