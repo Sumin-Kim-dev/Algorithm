338c9808552c3909dd122c29c24f787eaae67e15
Sumin-Kim-dev <sumin.kim.develop@gmail.com>
2022-08-10 20:00:32 +0900

    우주신과의 교감 : Gold 3(Prim)

--- src/BOJ1774.java
+++ src/BOJ1774.java
@@ -7,74 +7,33 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BOJ1774 {
-    int n, m;
+    final double INF = 2_000_000_000;
+    int n;
     long[][] points;
-    int[] sets;
-    PriorityQueue<Path> paths = new PriorityQueue<>(Comparator.comparingDouble(o -> o.dist));
-
-    static class Path {
-        int start, end;
-        double dist;
-
-        public Path(int start, int end, double dist) {
-            this.start = start;
-            this.end = end;
-            this.dist = dist;
-        }
-    }
+    double[][] dist;
 
     void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         points = new long[n][2];
-        sets = new int[n];
-        Arrays.fill(sets, -1);
-        setPoints(br);
-        setConnectedGods(br);
-    }
-
-    void setPoints(BufferedReader br) throws IOException {
-        StringTokenizer st;
+        dist = new double[n][n];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             points[i][0] = Long.parseLong(st.nextToken());
             points[i][1] = Long.parseLong(st.nextToken());
             for (int j = 0; j < i; j++) {
-                paths.add(new Path(i, j, dist(points[i], points[j])));
+                dist[i][j] = dist[j][i] = dist(points[i], points[j]);
             }
         }
-    }
-
-    void setConnectedGods(BufferedReader br) throws IOException {
-        StringTokenizer st;
         int cnt1, cnt2;
-        for (int i = 0; i < m; i++) {
+        while (m-- > 0) {
             st = new StringTokenizer(br.readLine());
             cnt1 = Integer.parseInt(st.nextToken()) - 1;
             cnt2 = Integer.parseInt(st.nextToken()) - 1;
-            union(cnt1, cnt2);
+            dist[cnt1][cnt2] = dist[cnt2][cnt1] = 0;
         }
-    }
-
-    int findSet(int i) {
-        if (sets[i] < 0) return i;
-        return sets[i] = findSet(sets[i]);
-    }
-
-    boolean union(int i, int j) {
-        int setI = findSet(i);
-        int setJ = findSet(j);
-        if (setI == setJ) return false;
-        if (-sets[setI] < -sets[setJ]) {
-            int temp = setI;
-            setI = setJ;
-            setJ = temp;
-        }
-        sets[setI] += sets[setJ];
-        sets[setJ] = setI;
-        return true;
     }
 
     double dist(long[] point1, long[] point2) {
@@ -82,22 +41,44 @@ public class BOJ1774 {
                 (point1[1] - point2[1]) * (point1[1] - point2[1]));
     }
 
-    double Kruskal() {
-        double minDist = 0;
-        int count = 0;
-        while (!paths.isEmpty() && count < n) {
-            Path curr = paths.poll();
-            if (union(curr.start, curr.end)) {
-                count++;
-                minDist += curr.dist;
+    double Prim() {
+        class God {
+            final int godNo;
+            final double minDist;
+
+            public God(int godNo, double minDist) {
+                this.godNo = godNo;
+                this.minDist = minDist;
             }
         }
-        return minDist;
+        double[] minDist = new double[n];
+        boolean[] checked = new boolean[n];
+        Arrays.fill(minDist, INF);
+
+        minDist[0] = 0;
+        PriorityQueue<God> gods = new PriorityQueue<>(Comparator.comparingDouble(g -> g.minDist));
+        double totalLength = 0;
+        gods.add(new God(0, 0));
+        while (!gods.isEmpty()) {
+            God curr = gods.poll();
+            if (checked[curr.godNo]) continue;
+            checked[curr.godNo] = true;
+            minDist[curr.godNo] = 0;
+            totalLength += curr.minDist;
+            for (int i = 0; i < n; i++) {
+                if (checked[i]) continue;
+                if (minDist[curr.godNo] + dist[curr.godNo][i] < minDist[i]) {
+                    minDist[i] = minDist[curr.godNo] + dist[curr.godNo][i];
+                    gods.add(new God(i, minDist[i]));
+                }
+            }
+        }
+        return totalLength;
     }
 
     void solution() throws IOException {
         input();
-        System.out.printf("%.2f", Kruskal());
+        System.out.printf("%.2f", Prim());
     }
 
     public static void main(String[] args) throws IOException {
