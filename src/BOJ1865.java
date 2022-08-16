abf8c71f59ea483dadbc00c3f58f76624abcd4a4
Sumin-Kim-dev <sumin.kim.develop@gmail.com>
2022-08-17 02:56:49 +0900

    웜홀 : Gold 3 (벨만-포드 풀이 1)

--- src/BOJ1865.java
+++ src/BOJ1865.java
@@ -1,19 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ1865 {
     static final int INF = 10_000_000;
     int n;
-    Integer[][] time;
+    ArrayList<Edge> time;
+    boolean[] checked;
+
+    static class Edge {
+        int start, end, time;
+
+        public Edge(int start, int end, int time) {
+            this.start = start;
+            this.end = end;
+            this.time = time;
+        }
+    }
 
     void input(BufferedReader br) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int w = Integer.parseInt(st.nextToken());
-        time = new Integer[n][n];
+        time = new ArrayList<>();
+        checked = new boolean[n];
         setRoads(m, br);
         setWormHoles(w, br);
     }
@@ -21,50 +35,50 @@ public class BOJ1865 {
     void setRoads(int m, BufferedReader br) throws IOException {
         StringTokenizer st;
         int s, e, t;
-        while (m-- > 0) {
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             s = Integer.parseInt(st.nextToken()) - 1;
             e = Integer.parseInt(st.nextToken()) - 1;
             t = Integer.parseInt(st.nextToken());
-            if (time[s][e] == null || t < time[s][e])
-                time[s][e] = time[e][s] = t;
+            time.add(new Edge(s, e, t));
+            time.add(new Edge(e, s, t));
         }
     }
 
     void setWormHoles(int w, BufferedReader br) throws IOException {
         StringTokenizer st;
         int s, e, t;
-        while (w-- > 0) {
+        for (int i = 0; i < w; i++) {
             st = new StringTokenizer(br.readLine());
             s = Integer.parseInt(st.nextToken()) - 1;
             e = Integer.parseInt(st.nextToken()) - 1;
             t = -Integer.parseInt(st.nextToken());
-            if (time[s][e] == null || t < time[s][e])
-                time[s][e] = t;
+            time.add(new Edge(s, e, t));
         }
     }
 
-    String FloydWarshall() {
-        int[][] minTime = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            if (time[i][i] != null && time[i][i] < 0) return "YES";
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                if (time[i][j] == null)
-                    minTime[i][j] = INF;
-                else minTime[i][j] = time[i][j];
+    boolean BellmanFord(int start) {
+        int[] minTime = new int[n];
+        Arrays.fill(minTime, INF);
+        minTime[start] = 0;
+        checked[start] = true;
+        for (int k = 1; k <= n; k++) {
+            for (Edge e : time) {
+                if (minTime[e.start] == INF) continue;
+                if (minTime[e.start] + e.time < minTime[e.end]) {
+                    if (k == n) return true;
+                    minTime[e.end] = minTime[e.start] + e.time;
+                    checked[e.end] = true;
+                }
             }
         }
+        return false;
+    }
 
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (minTime[i][k] == INF || minTime[k][j] == INF) continue;
-                    if (minTime[i][k] + minTime[k][j] < minTime[i][j])
-                        minTime[i][j] = minTime[i][k] + minTime[k][j];
-                }
-                if (minTime[i][i] < 0) return "YES";
-            }
+    String answer() {
+        for (int i = 0; i < n; i++) {
+            if (checked[i]) continue;
+            if (BellmanFord(i)) return "YES";
         }
         return "NO";
     }
@@ -74,7 +88,7 @@ public class BOJ1865 {
         int tc = Integer.parseInt(br.readLine());
         while (tc-- > 0) {
             input(br);
-            System.out.println(FloydWarshall());
+            System.out.println(answer());
         }
     }
 
