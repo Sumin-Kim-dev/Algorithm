c5fe2dcf7c079a86d896b08c3a748c73ea368e97
Sumin-Kim-dev <sumin.kim.develop@gmail.com>
2022-08-21 02:53:24 +0900

    크게 만들기 : Gold 3 (계수정렬의 아이디어)

--- src/BOJ2812.java
+++ src/BOJ2812.java
@@ -1,11 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ2812 {
     int n, k;
     int[] numbers;
+    Queue<Integer>[] index;
 
     void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,32 +14,46 @@ public class BOJ2812 {
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
         numbers = new int[n];
+        index = new Queue[10];
+        for (int i = 0; i < 10; i++)
+            index[i] = new LinkedList<>();
         String number = br.readLine();
         for (int i = 0; i < n; i++) {
             numbers[i] = number.charAt(i) - '0';
+            index[numbers[i]].offer(i);
         }
     }
 
-    int greedy(int currIndex) {
-        int max = 0, index = currIndex;
-        if (currIndex + k == n) return n; // currIndex + k는 1씩 증가함
-        for (int i = currIndex; i <= currIndex + k; i++) {
-            if (numbers[i] > max) {
-                max = numbers[i];
-                index = i;
-            }
+    int currIndex = 0;
+
+    int greedy() {
+        if (k == 0) {
+            return numbers[currIndex++];
         }
-        k -= (index - currIndex);
-        System.out.print(max);
-        return index + 1;
+        int curr;
+        for (int i = 9; i >= 0; i--) {
+            curr = -1;
+            while (!index[i].isEmpty()) {
+                curr = index[i].peek();
+                if (curr >= currIndex) break;
+                index[i].poll();
+            }
+            if (curr < currIndex || curr > currIndex + k) continue;
+            k -= (curr - currIndex);
+            currIndex = curr + 1;
+            return i;
+        }
+        return 0;
     }
 
     void solution() throws IOException {
         input();
-        int currIndex = 0;
-        while (k >= 0 && currIndex < n) {
-            currIndex = greedy(currIndex);
+        StringBuilder sb = new StringBuilder();
+        int length = n - k;
+        for (int i = 0; i < length; i++) {
+            sb.append(greedy());
         }
+        System.out.println(sb);
     }
 
     public static void main(String[] args) throws IOException {
