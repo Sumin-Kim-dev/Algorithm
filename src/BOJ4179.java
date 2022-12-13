0a43935cdb4094e01ac972482d7f1c49935bf421
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-12-14 00:27:56 +0900

    불! : Gold 4

--- src/BOJ4179.java
+++ src/BOJ4179.java
@@ -9,7 +9,6 @@ public class BOJ4179 {
     int r, c;
     char[][] maze;
     Queue<int[]> queue = new LinkedList<>();
-    boolean[][][] isVisited;
 
     public static void main(String[] args) throws IOException {
         new BOJ4179().io();
@@ -21,15 +20,13 @@ public class BOJ4179 {
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
         maze = new char[r][c];
-        isVisited = new boolean[r][c][2];
         for (int i = 0; i < r; i++) {
             String row = br.readLine();
             for (int j = 0; j < c; j++) {
-                 maze[i][j] = row.charAt(j);
-                 if (maze[i][j] == 'J') {
-                     isVisited[i][j][0] = true;
-                     queue.offer(new int[]{i, j, 0});
-                 }
+                maze[i][j] = row.charAt(j);
+                if (maze[i][j] == 'F') {
+                    queue.offer(new int[]{i, j, 1});
+                }
             }
         }
         int time = solution();
@@ -40,27 +37,31 @@ public class BOJ4179 {
         final int[][] DIR = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (maze[i][j] == 'F') {
-                    queue.offer(new int[]{i, j, 1});
-                    isVisited[i][j][1] = true;
+                if (maze[i][j] == 'J') {
+                    if (i == 0 || i == r - 1 || j == 0 || j == c - 1) return 1;
+                    queue.offer(new int[]{i, j, 0});
                 }
             }
         }
-        int time = 1;
-        while(!queue.isEmpty()) {
+        int time = 2;
+        while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 int[] curr = queue.poll();
-                if (curr[2] == 0 && isVisited[curr[0]][curr[1]][1]) continue;
-                if (curr[2] == 0 && (curr[0] == 0 || curr[0] == r - 1 || curr[1] == 0 || curr[1] == c - 1)) {
-                    return time;
-                }
                 for (int[] dir : DIR) {
                     int[] next = {curr[0] + dir[0], curr[1] + dir[1], curr[2]};
                     if (next[0] < 0 || next[0] >= r || next[1] < 0 || next[1] >= c) continue;
-                    if (maze[next[0]][next[1]] == '#') continue;
-                    if (isVisited[next[0]][next[1]][next[2]] || isVisited[next[0]][next[1]][1]) continue;
-                    isVisited[next[0]][next[1]][next[2]] = true;
+                    if (maze[next[0]][next[1]] == '#' || maze[next[0]][next[1]] == 'F') continue;
+                    if (next[2] == 0) {
+                        if (next[0] == 0 || next[0] == r - 1 || next[1] == 0 || next[1] == c - 1) {
+                            return time;
+                        }
+                        if (maze[next[0]][next[1]] == 'J') continue;
+                        maze[next[0]][next[1]] = 'J';
+                    }
+                    if (next[2] == 1) {
+                        maze[next[0]][next[1]] = 'F';
+                    }
                     queue.offer(next);
                 }
             }
