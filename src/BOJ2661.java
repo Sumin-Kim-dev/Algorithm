6a9b71478695c040e99796233efd3833d762ade6
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-12-15 23:55:11 +0900

    좋은수열 : Gold 4

---
+++ src/BOJ2661.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2661 {
+    int n;
+    int[] seq;
+    String goodSeq = "";
+
+    public static void main(String[] args) throws IOException {
+        new BOJ2661().io();
+    }
+
+    void io() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        seq = new int[n];
+        backtracking(0);
+        System.out.println(goodSeq);
+    }
+
+    void backtracking(int depth) {
+        if (!goodSeq.isEmpty()) return;
+        if (depth == n) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++) {
+                sb.append(seq[i]);
+            }
+            goodSeq = sb.toString();
+            return;
+        }
+        for (int i = 1; i <= 3; i++) {
+            if (depth > 0 && seq[depth - 1] == i) continue;
+            seq[depth] = i;
+            if (isGood(seq, depth)) backtracking(depth + 1);
+            seq[depth] = 0;
+        }
+    }
+
+    boolean isGood(int[] seq, int depth) {
+        for (int i = 1; i <= (depth + 1) / 2; i++) {
+            boolean isBad = true;
+            for (int j = 0; j < i; j++) {
+                isBad &= (seq[depth - j] == seq[depth - i - j]);
+            }
+            if (isBad) return false;
+        }
+        return true;
+    }
+}
