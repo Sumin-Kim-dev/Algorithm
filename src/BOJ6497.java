4fcceca411daa2bec0cdd518e806f57ba59c795b
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-12-24 01:32:54 +0900

    전력난 : Gold 4

--- src/BOJ6497.java
+++ src/BOJ6497.java
@@ -16,45 +16,52 @@ public class BOJ6497 {
         int m = Integer.parseInt(st.nextToken());
         if (n == 0 && m == 0) return false;
         int sum = 0;
-        List<Road>[] adj = new List[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<>();
-        }
+        PriorityQueue<Road> pq = new PriorityQueue<>();
         while (m-- > 0) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             int z = Integer.parseInt(st.nextToken());
-            adj[x].add(new Road(y, z));
-            adj[y].add(new Road(x, z));
+            pq.add(new Road(x, y, z));
             sum += z;
         }
-        System.out.println(solution(n, sum, adj));
+        System.out.println(solution(n, sum, pq));
         return true;
     }
 
-    int solution(int n, int sum, List<Road>[] adj) {
+    int solution(int n, int sum, PriorityQueue<Road> pq) {
         int cost = sum;
-        boolean[] isSelected = new boolean[n];
-        PriorityQueue<Road> pq = new PriorityQueue<>();
-        pq.offer(new Road(0, 0));
-        while (!pq.isEmpty()) {
+        int cnt = 1;
+        int[] set = new int[n];
+        Arrays.fill(set, -1);
+        while (!pq.isEmpty() && cnt < n) {
             Road curr = pq.poll();
-            if (isSelected[curr.end]) continue;
-            isSelected[curr.end] = true;
-            cost -= curr.dist;
-            for (Road road : adj[curr.end]) {
-                if (isSelected[road.end]) continue;
-                pq.offer(road);
+            if (union(curr.start, curr.end, set)) {
+                cnt++;
+                cost -= curr.dist;
             }
         }
         return cost;
     }
 
-    static class Road implements Comparable<Road> {
-        int end, dist;
+    boolean union(int s, int e, int[] set) {
+        int setS = find(s, set);
+        int setE = find(e, set);
+        if (setS == setE) return false;
+        set[setE] = setS;
+        return true;
+    }
 
-        public Road(int end, int dist) {
+    int find(int a, int[] set) {
+        if (set[a] < 0) return a;
+        return set[a] = find(set[a], set);
+    }
+
+    static class Road implements Comparable<Road> {
+        int start, end, dist;
+
+        public Road(int start, int end, int dist) {
+            this.start = start;
             this.end = end;
             this.dist = dist;
         }
