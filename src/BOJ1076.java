35eb5254a6af0f9d688d0240a1e235aaafd9f0ea
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-01-04 10:31:14 +0900

    저항 : Bronze 2

---
+++ src/BOJ1076.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1076 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Color[] resist = new Color[3];
+        for (int i = 0; i < resist.length; i++) {
+            resist[i] = Color.valueOf(br.readLine().toUpperCase());
+        }
+        System.out.println((resist[0].value * 10 + resist[1].value) * resist[2].product);
+    }
+
+    enum Color {
+        BLACK, BROWN, RED, ORANGE, YELLOW,
+        GREEN, BLUE, VIOLET, GREY, WHITE;
+
+        final long value;
+        final long product;
+
+        Color() {
+            value = ordinal();
+            product = pow(value);
+        }
+
+        private long pow(long value) {
+            long init = 1;
+            for (int i = 0; i < value; i++) {
+                init *= 10;
+            }
+            return init;
+        }
+    }
+}
