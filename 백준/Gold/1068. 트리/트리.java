d2e50c2c2352af9d69d0f95f64501de514c09914
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-01 20:36:17 +0900

    [Gold V] Title: 트리, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    int n;
+    int root;
+    List<Integer>[] children;
+
+    public static void main(String[] args) throws IOException {
+        new Main().io();
+    }
+
+    void io() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        children = new List[n];
+        for (int i = 0; i < n; i++) {
+            children[i] = new ArrayList<>();
+        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int parent = Integer.parseInt(st.nextToken());
+            if (parent == -1) root = i;
+            else children[parent].add(i);
+        }
+        int erase = Integer.parseInt(br.readLine());
+        System.out.println(solution(erase));
+    }
+
+    int solution(int erase) {
+        if (erase == root) return 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(root);
+        int leafs = 0;
+        while (!stack.isEmpty()) {
+            int curr = stack.pop();
+            int count = 0;
+            for (int child : children[curr]) {
+                if (child == erase) continue;
+                stack.push(child);
+                count++;
+            }
+            if (count == 0) leafs++;
+        }
+        return leafs;
+    }
+}
