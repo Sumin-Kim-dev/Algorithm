d3daed926f546e0b8beeb7838621c4ae10cb910c
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-01 20:40:56 +0900

    [Gold III] Title: IQ Test, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Gold/1111. IQ Test/IQ Test.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		int arr[] = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		bw.write(ans(arr, n));
+		bw.close();
+	}
+
+	static String ans(int[] arr, int n) {
+		if (n == 1)
+			return "A";
+		if (n == 2 && arr[0] != arr[1])
+			return "A";
+		if (arr[0] == arr[1]) {
+			for (int i = 1; i < n; i++) {
+				if (arr[i] != arr[0])
+					return "B";
+			}
+			return Integer.toString(arr[0]);
+		}
+		if ((arr[2] - arr[1]) % (arr[1] - arr[0]) != 0)
+			return "B";
+		int a = (arr[2] - arr[1]) / (arr[1] - arr[0]);
+		int b = (arr[1] * arr[1] - arr[0] * arr[2]) / (arr[1] - arr[0]);
+		for (int i = 1; i < n; i++) {
+			int expect = a * arr[i - 1] + b;
+			if (expect != arr[i])
+				return "B";
+		}
+		return Integer.toString(a * arr[n - 1] + b);
+	}
+}
