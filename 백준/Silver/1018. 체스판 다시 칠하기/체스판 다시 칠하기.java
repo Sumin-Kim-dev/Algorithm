62d4895bee1f46fdfdb51c69c66111234ee4058b
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-01 20:30:54 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 136 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int board[][] = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			String boardRow = br.readLine();
+			for (int j = 0; j < M; j++) {
+				if (boardRow.charAt(j) == 'W')
+					board[i][j] = 0;
+				else
+					board[i][j] = 1;
+			}
+		}
+
+		int min = 32;
+
+		for (int i = 0; i < N - 7; i++) {
+			for (int j = 0; j < M - 7; j++) {
+				int repaint = 0;
+				for (int k = 0; k < 8; k++) {
+					for (int l = 0; l < 8; l++) {
+						repaint += ((k + l) % 2) ^ board[i + k][j + l];
+					}
+				}
+				if (repaint > 32)
+					repaint = 64 - repaint;
+				if (repaint < min)
+					min = repaint;
+			}
+		}
+		bw.write("" + min);
+		bw.close();
+	}
+
+}
