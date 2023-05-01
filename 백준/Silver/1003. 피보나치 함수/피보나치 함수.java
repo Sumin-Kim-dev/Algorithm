fda8a80ec8bab1459b5ab17605cebaee4e463d4f
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-01 20:27:51 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Main {
+
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int T = Integer.parseInt(br.readLine());
+		int n;
+		for (int i = 0; i < T; i++) {
+			n = Integer.parseInt(br.readLine());
+			printFibonacci(n);
+		}
+		bw.write(sb.toString());
+		bw.close();
+	}
+
+	static void printFibonacci(int n) {
+		int zeros_nMinus1 = -1, zeros_n = 1, temp;
+		for (int i = 1; i <= n; i++) {
+			temp = zeros_n;
+			zeros_n = zeros_nMinus1 + zeros_n;
+			zeros_nMinus1 = temp;
+		}
+		sb.append(zeros_n);
+		sb.append(" ");
+		sb.append(zeros_nMinus1 + zeros_n);
+		sb.append("\n");
+	}
+}
