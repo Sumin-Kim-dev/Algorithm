327a652ee41a32440c029254cba98d78111de72a
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-02 03:32:07 +0900

    [Silver IV] Title: 하키, Time: 128 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Silver/1358. 하키/하키.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		int W = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		int Y = Integer.parseInt(st.nextToken());
+		int P = Integer.parseInt(st.nextToken());
+
+		int x, y, players = 0;
+		for (int i = 0; i < P; i++) {
+			st = new StringTokenizer(br.readLine());
+			x = Integer.parseInt(st.nextToken());
+			y = Integer.parseInt(st.nextToken());
+			if (isIn(x, y, W, H, X, Y))
+				players++;
+		}
+		bw.write(players + "");
+		bw.close();
+	}
+
+	static boolean isIn(int x, int y, int W, int H, int X, int Y) {
+		if (x < X)
+			return (X - x) * (X - x) + (Y + H / 2 - y) * (Y + H / 2 - y) <= (H / 2) * (H / 2);
+		else if (x > X + W)
+			return (X + W - x) * (X + W - x) + (Y + H / 2 - y) * (Y + H / 2 - y) <= (H / 2) * (H / 2);
+		else
+			return y >= Y && y <= Y + H;
+	}
+}
