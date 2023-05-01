46e207953ba5b166246d0ffea686055b1023fe17
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-01 20:29:26 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 120 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    void io() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while(t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            sb.append(solution(y - x)).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    int solution(int x) {
+        if (x <= 3) return x;
+        int max = (int) Math.sqrt(x);
+        return (x - 1) / max + max;
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().io();
+    }
+}
