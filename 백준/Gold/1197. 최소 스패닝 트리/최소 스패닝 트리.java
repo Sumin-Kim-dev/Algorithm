03a898214dbd5506f6a9dcbbdefa3694c735d867
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-01 20:43:09 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 724 ms, Memory: 54412 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    int v;
+    ArrayList<Edge>[] adj;
+
+    class Edge implements Comparable<Edge> {
+        int end, weight;
+
+        Edge(int end, int weight) {
+            this.end = end;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            if (this.weight > o.weight) return 1;
+            if (this.weight < o.weight) return -1;
+            return this.end - o.end;
+        }
+    }
+
+    void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        adj = new ArrayList[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        while (e-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            int end = Integer.parseInt(st.nextToken()) - 1;
+            int weight = Integer.parseInt(st.nextToken());
+            adj[start].add(new Edge(end, weight));
+            adj[end].add(new Edge(start, weight));
+        }
+    }
+
+    int Prim() {
+        int totalWeight = 0;
+        boolean[] isSelected = new boolean[v];
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(0, 0));
+        while (!pq.isEmpty()) {
+            Edge currEdge = pq.poll();
+            int curr = currEdge.end;
+            if (isSelected[curr]) continue;
+            isSelected[curr] = true;
+            totalWeight += currEdge.weight;
+            for (Edge e : adj[curr]) {
+                if (isSelected[e.end]) continue;
+                pq.add(e);
+            }
+        }
+        return totalWeight;
+    }
+
+    void solution() throws IOException {
+        input();
+        System.out.println(Prim());
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+}
