472c6ddfc75b2735f530e0c71d5946aab3561e5b
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-03 00:29:16 +0900

    [Gold IV] Title: 부분합, Time: 300 ms, Memory: 24468 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = Integer.parseInt(st.nextToken());
+		bw.write(minLength(a, s) + "");
+		bw.close();
+	}
+
+	static int minLength(int[] a, int s) {
+		int l = 0, f = 1;
+		int sum = a[0], minLength = a.length + 1;
+		while (l < f) {
+			if (sum >= s) {
+				if (f - l < minLength)
+					minLength = f - l;
+				sum -= a[l];
+				l++;
+			}
+			if (sum < s) {
+				if (f == a.length)
+					break;
+				sum += a[f];
+				f++;
+			}
+		}
+		if (minLength == a.length + 1)
+			return 0;
+		return minLength;
+	}
+}
