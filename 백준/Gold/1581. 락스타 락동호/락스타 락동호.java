7fca8875861d055a4cea19048c0b68f37a4415b2
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-03 00:11:12 +0900

    [Gold IV] Title: 락스타 락동호, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Gold/1581. 락스타 락동호/락스타 락동호.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    int ff, fs, sf, ss;
+
+    void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        ff = Integer.parseInt(st.nextToken());
+        fs = Integer.parseInt(st.nextToken());
+        sf = Integer.parseInt(st.nextToken());
+        ss = Integer.parseInt(st.nextToken());
+    }
+
+    int ans() {
+        if(ff == 0 && fs == 0) return ss + Math.min(sf, 1);
+        if(fs == 0) return ff;
+        int changeTempo;
+        if(fs > sf) changeTempo = 2 * sf + 1;
+        else changeTempo = 2 * fs;
+        return ff + ss + changeTempo;
+    }
+
+    void solution() throws IOException {
+        input();
+        System.out.println(ans());
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+}
