62ff0e3dd2563fd1d7db0e696716ea719f918d9c
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-03 00:29:06 +0900

    [Silver II] Title: 종이의 개수, Time: 1736 ms, Memory: 378716 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int number[] = new int[3];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		int paper[][] = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				paper[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		number(paper);
+		bw.write(number[0] + "\n" + number[1] + "\n" + number[2]);
+		bw.close();
+	}
+
+	static void number(int paper[][]) {
+		int N = paper.length;
+		if (isEqual(paper) != DEFAULT)
+			number[isEqual(paper) + 1]++;
+
+		else {
+			int split[][][] = new int[9][N / 3][N / 3];
+			for (int i = 0; i < N / 3; i++) {
+				for (int j = 0; j < 9; j++) {
+					split[j][i] = Arrays.copyOfRange(paper[i + (j / 3) * N / 3], (j % 3) * N / 3, (j % 3 + 1) * N / 3);
+				}
+			}
+			for (int j = 0; j < 9; j++)
+				number(split[j]);
+		}
+	}
+
+	static final int DEFAULT = -2;
+
+	static int isEqual(int paper[][]) {
+		int N = paper.length;
+		int x = paper[0][0];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++)
+				if (paper[i][j] != x)
+					return DEFAULT;
+		}
+		return x;
+	}
+}
