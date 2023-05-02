ee5726cbc8e652350a25544ecab51049d0ee94a5
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-03 00:29:30 +0900

    [Silver IV] Title: 카드, Time: 184 ms, Memory: 28320 KB -BaekjoonHub

---
+++ 백준/Silver/1835. 카드/카드.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    int n;
+    Queue<Integer> cards = new LinkedList<>();
+    HashMap<Integer, Integer> cardOrder = new HashMap<>();
+
+    void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            cards.add(i);
+        }
+    }
+
+    void repeat() {
+        for (int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++)
+                cards.add(cards.poll());
+            cardOrder.put(cards.poll(), i);
+        }
+    }
+
+    void print() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= n; i++) {
+            sb.append(cardOrder.get(i)).append(' ');
+        }
+        bw.write(sb.toString());
+        bw.close();
+    }
+
+    void solution() throws IOException {
+        input();
+        repeat();
+        print();
+    }
+    public static void main(String[] args) throws IOException{
+        new Main().solution();
+    }
+}
