cb99d146f648b3b69853d31079bde4713002039b
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-03 00:10:43 +0900

    [Bronze I] Title: 평균, Time: 284 ms, Memory: 19968 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int score = 0;
+        int sum = 0;
+        int max = 0;
+        for(int i=0; i<N; i++) {
+            score = in.nextInt();
+            sum += score;
+            if(score >= max) max = score;
+        }
+        System.out.println(100.0 * sum / max / N);
+    }
+}
