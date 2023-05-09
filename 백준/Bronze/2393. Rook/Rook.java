dab9e145ea147bca80f281830d80be7ff3a3f627
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-09 22:03:36 +0900

    [Bronze V] Title: Rook, Time: 112 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb
+            .append("  ___  ___  ___").append('\n')
+            .append("  | |__| |__| |").append('\n')
+            .append("  |           |").append('\n')
+            .append("   \\_________/").append('\n')
+            .append("    \\_______/").append('\n')
+            .append("     |     |").append('\n')
+            .append("     |     |").append('\n')
+            .append("     |     |").append('\n')
+            .append("     |     |").append('\n')
+            .append("     |_____|").append('\n')
+            .append("  __/       \\__").append('\n')
+            .append(" /             \\").append('\n')
+            .append("/_______________\\").append('\n');
+        System.out.println(sb);
+    }
+}
