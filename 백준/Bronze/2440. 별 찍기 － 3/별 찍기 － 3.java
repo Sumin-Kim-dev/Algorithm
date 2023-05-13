eb98d991fa2ec92cfa5014c9d486bddff230ba6f
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-14 01:47:44 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = n; i >= 1; i--) {
+            sb.append("*".repeat(i)).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
