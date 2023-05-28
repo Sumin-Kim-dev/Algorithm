51e30e137fea9ef94f630c77865964b72f229fbe
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-05-29 00:58:14 +0900

    [Bronze III] Title: 대지, Time: 292 ms, Memory: 27416 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int minX = 20001;
+        int minY = 20001;
+        int maxX = -20001;
+        int maxY = -20001;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if (x < minX) minX = x;
+            if (x > maxX) maxX = x;
+            if (y < minY) minY = y;
+            if (y > maxY) maxY = y;
+        }
+        System.out.println((maxX - minX) * (maxY - minY));
+    }
+}
