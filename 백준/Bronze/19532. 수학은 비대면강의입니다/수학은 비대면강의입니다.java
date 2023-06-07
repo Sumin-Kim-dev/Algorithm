145c7bf7e937c29fec9ce558bccd39b1bc250180
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-06-07 13:40:17 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 136 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+        System.out.printf("%d %d", (c * e - f * b) / (a * e - d * b), (c * d - f * a) / (b * d - e * a));
+    }
+}
