1644349dfac13fa538c1362f964e2a8d76203801
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-08 15:47:41 +0900

    [Gold IV] Title: 치즈, Time: 112 ms, Memory: 13256 KB -BaekjoonHub

---
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int n;
+	static int m;
+	static int[][] map;
+	static int[][] before;
+	static int count;
+	
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		map = new int[n][m];
+		before = new int[n][m];
+		count = 0;
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 1) count++;
+			}
+		}
+		
+		int t = 0;
+		int answer = 0;
+		while (count > 0) {
+			t++;
+			before = new int[n][m];
+			for (int i = 0; i < n; i++) {
+				before[i] = Arrays.copyOf(map[i], m);
+			}
+			bfs();
+			answer = melt();
+			map = before;
+		}
+		System.out.println(t);
+		System.out.println(answer);
+	}
+
+	private static int melt() {
+		int melt = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (map[i][j] == 3) {
+					before[i][j] = 0;
+					melt++;
+					count--;
+				}
+			}
+		}
+		return melt;
+	}
+
+	private static void bfs() {
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] {0, 0});
+		map[0][0] = 2;
+		while (!queue.isEmpty()) {
+			int[] curr = queue.poll();
+			for (int d = 0; d < 4; d++) {
+				int nr = curr[0] + dr[d];
+				int nc = curr[1] + dc[d];
+				if (!isIn(nr, nc)) continue;
+				if (map[nr][nc] == 0) {
+					queue.offer(new int[] {nr, nc});
+					map[nr][nc] = 2;
+				} else if (map[nr][nc] == 1) {
+					map[nr][nc] = 3;
+				}
+			}
+		}
+	}
+
+	private static boolean isIn(int r, int c) {
+		return r >= 0 && r < n && c >= 0 && c < m;
+	}
+}
