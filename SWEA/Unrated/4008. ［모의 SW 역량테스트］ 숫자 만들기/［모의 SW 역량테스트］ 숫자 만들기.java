4aa3f2ebb867736b2e0dbcf8bc09b19bb72b35d8
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-10 14:28:18 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 112 ms, Memory: 21,480 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -6,12 +6,11 @@ import java.util.StringTokenizer;
 public class Solution {
 	
 	static int n;
-	static int[] ops = new int[4];
+	static int plus, minus, mul, divide;
 	static int[] nums;
-	
 	static int max;
 	static int min;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int tc = Integer.parseInt(br.readLine());
@@ -19,9 +18,11 @@ public class Solution {
 		for (int t = 1; t <= tc; t++) {
 			n = Integer.parseInt(br.readLine());
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < 4; i++) {
-				ops[i] = Integer.parseInt(st.nextToken());
-			}
+			plus = Integer.parseInt(st.nextToken());
+			minus = Integer.parseInt(st.nextToken());
+			mul = Integer.parseInt(st.nextToken());
+			divide = Integer.parseInt(st.nextToken());
+			
 			nums = new int[n];
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < n; i++) {
@@ -29,37 +30,29 @@ public class Solution {
 			}
 			max = -100_000_001;
 			min = 100_000_001;
-			dfs(0, nums[0]);
+			dfs(1, nums[0], plus, minus, mul, divide);
 			sb.append("#" + t + " " + (max - min) + "\n");
 		}
 		System.out.println(sb);
 	}
 
-	private static void dfs(int cnt, int curr) {
-		if (cnt == n - 1) {
+	private static void dfs(int cnt, int curr, int plus, int minus, int mul, int divide) {
+		if (cnt == n) {
 			max = Math.max(max, curr);
 			min = Math.min(min, curr);
 			return;
 		}
-		if (ops[0] > 0) {
-			ops[0]--;
-			dfs(cnt + 1, curr + nums[cnt + 1]);
-			ops[0]++;
+		if (plus > 0) {
+			dfs(cnt + 1, curr + nums[cnt], plus - 1, minus, mul, divide);
 		}
-		if (ops[1] > 0) {
-			ops[1]--;
-			dfs(cnt + 1, curr - nums[cnt + 1]);
-			ops[1]++;
+		if (minus > 0) {
+			dfs(cnt + 1, curr - nums[cnt], plus, minus - 1, mul, divide);
 		}
-		if (ops[2] > 0) {
-			ops[2]--;
-			dfs(cnt + 1, curr * nums[cnt + 1]);
-			ops[2]++;
+		if (mul > 0) {
+			dfs(cnt + 1, curr * nums[cnt], plus, minus, mul - 1, divide);
 		}
-		if (ops[3] > 0) {
-			ops[3]--;
-			dfs(cnt + 1, curr / nums[cnt + 1]);
-			ops[3]++;
+		if (divide > 0) {
+			dfs(cnt + 1, curr / nums[cnt], plus, minus, mul, divide - 1);
 		}
 	}
 }
