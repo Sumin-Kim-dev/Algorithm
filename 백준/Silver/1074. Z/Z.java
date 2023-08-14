9eba1b4404b0ee91b2a0bd29989808eb6b6bf237
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-14 17:29:45 +0900

    [Silver I] Title: Z, Time: 112 ms, Memory: 12848 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,40 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-	static int n, r, c;
-	static int answer = 0;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int r = sc.nextInt();
+		int c = sc.nextInt();
 		
-		n = sc.nextInt();
-		r = sc.nextInt();
-		c = sc.nextInt();
-		
-		recur(0, 0, (int)Math.pow(2, n));
-
-	}
-	static void recur(int x, int y, int n) {
-		if (x == r && y == c) {
-			System.out.println(answer);
-			System.exit(0);
+		int answer = 0;
+		for (int i = 0; i < n; i++) {
+			answer |= (r & (1 << i)) << (i + 1);
+			answer |= (c & (1 << i)) << i;
 		}
-		
-		if (n == 1) {
-			answer += 1;
-			return;
-		}
-	    
-		if(!(x <= r && r < x + n && y <= c && c < y + n)) {
-			answer += n * n;
-			return;
-		}
-		
-		int length = n / 2;
-		
-		recur(x, y, length);
-		recur(x, y + length, length);
-		recur(x + length, y, length);
-		recur(x + length, y + length, length);
+		System.out.println(answer);
 	}
 
 }
