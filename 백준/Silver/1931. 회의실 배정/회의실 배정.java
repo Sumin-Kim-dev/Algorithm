4f8932baa5febee24878dffd1da5f4258a3ab401
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-14 13:17:49 +0900

    [Silver I] Title: 회의실 배정, Time: 600 ms, Memory: 46252 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,35 +1,32 @@
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int n;
-	static List<int[]> list = new LinkedList<int[]>();
-	static int start, end;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[][] times = new int[n][2];
 		for (int i = 0; i < n; i++) {
-			start = sc.nextInt();
-			end = sc.nextInt();
-			list.add(new int[] {start, end});
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			times[i][0] = Integer.parseInt(st.nextToken());
+			times[i][1] = Integer.parseInt(st.nextToken());
 		}
-		
-		Collections.sort(list, Comparator.comparing((int[] arr) -> arr[1]).thenComparing(arr -> arr[0]));
-		
-		int answer = 0;
-		int endtime = 0;
-		
-		for (int[] meeting : list) {
-            if (meeting[0] >= endtime) {
-                endtime = meeting[1];
-                answer++;
-            }
-        }
-		System.out.println(answer);
+		Arrays.sort(times, (o1, o2) -> {
+			if (o1[1] != o2[1]) return Integer.compare(o1[1], o2[1]);
+			return Integer.compare(o1[0], o2[0]);
+		});
+		int count = 0;
+		int end = 0;
+		for (int i = 0; i < n; i++) {
+			if (times[i][0] >= end) {
+				count++;
+				end = times[i][1];
+			}
+		}
+		System.out.println(count);
 	}
 }
