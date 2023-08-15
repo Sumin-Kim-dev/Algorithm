a86615f6012282d55fd762188ba014c9f88daa05
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-15 17:00:46 +0900

    [Silver II] Title: 색종이 만들기, Time: 116 ms, Memory: 13152 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,44 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int spaces[][];
-	static int white, green;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		spaces = new int [n][n];
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				spaces[i][j] = sc.nextInt();
-			}
-		}
-		
-		makeSpace(0, 0, n);
-		System.out.println(white);
-		System.out.println(green);
-	}
-	
-	private static void makeSpace(int sr, int sc, int size) {
-		
-		int sum = 0;
-		for (int r = sr; r < sr + size; r++) {
-			for (int c = sc; c < sc + size; c++) {
-				sum += spaces[r][c];
-			}
-		}
-		
-		if(sum == 0) {	// 모두 하얀색인 공간
-			white ++;
-		}else if(sum == size * size) {	// 모두 초록색인 공간
-			green++;
-		}else {	// 두 색이 섞여있는 공간
-			int half = size/2;
-			makeSpace(sr, sc, half);	//1
-			makeSpace(sr, sc + half, half);		//2
-			makeSpace(sr + half, sc, half);		//3
-			makeSpace(sr + half, sc + half, half);		//3
-		}
-	}
 
+    static int n;
+    static int[][] paper;
+
+    static int white;
+    static int blue;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        paper = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        count(0, 0, n);
+        System.out.println(white);
+        System.out.println(blue);
+    }
+
+    private static void count(int r, int c, int size) {
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                sum += paper[r + i][c + j];
+            }
+        }
+        if (sum == 0) {
+            white++;
+            return;
+        }
+        if (sum == size * size) {
+            blue++;
+            return;
+        }
+        count(r, c, size / 2);
+        count(r, c + size / 2, size / 2);
+        count(r + size / 2, c, size / 2);
+        count(r + size / 2, c + size / 2, size / 2);
+    }
 }
