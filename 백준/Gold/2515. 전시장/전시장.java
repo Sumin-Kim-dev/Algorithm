8768b86e87371561be5b7ca67f48f4c7a49a4fb5
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-16 09:32:45 +0900

    [Gold II] Title: 전시장, Time: 548 ms, Memory: 236140 KB -BaekjoonHub

--- 백준/Gold/2515. 전시장/전시장.java
+++ 백준/Gold/2515. 전시장/전시장.java
@@ -1,48 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	
 	static int n;
 	static int s;
-	static int[][] pictures;
+	static int[] pictures;
 	static int[] price;
+	
+	static int min;
+	static int max;
+	
+	public static final int MAX = 20000000;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		s = Integer.parseInt(st.nextToken());
-		pictures = new int[n][2];
-		price = new int[n + 1];
+		pictures = new int[MAX + 1];
+		price = new int[MAX + 1];
+		min = MAX;
+		max = 0;
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			pictures[i][0] = Integer.parseInt(st.nextToken());
-			pictures[i][1] = Integer.parseInt(st.nextToken());
+			int h = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			if (h < min) min = h;
+			if (h > max) max = h;
+			if (pictures[h] < c) pictures[h] = c;
 		}
 		
-		Arrays.sort(pictures, (o1, o2) -> {
-			if (o1[0] != o2[0]) return Integer.compare(o1[0], o2[0]);
-			return Integer.compare(o1[1], o2[1]);
-		});
-		
-		for (int i = n - 1; i >= 0; i--) {
-			price[i] = Math.max(price[i + 1], price[find(pictures[i][0] + s)] + pictures[i][1]);
+		price[max] = pictures[max];
+		for (int h = max; h >= min; h--) {
+			price[h] = Math.max(price[h + 1], price[h + s] + pictures[h]);
 		}
-		System.out.println(price[0]);
-	}
-	
-	private static int find(int h) {
-		int start = 0;
-		int end = n;
-		while (start + 1 < end) {
-			int mid = (start + end) / 2;
-			if (pictures[mid][0] >= h) end = mid;
-			else start = mid;
-		}
-		return end;
+		System.out.println(price[min]);
 	}
 }
