58405c74e4347fd7d1243520e399e934292b2a35
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-17 14:42:55 +0900

    [Gold II] Title: 빵집, Time: 336 ms, Memory: 42116 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -1,44 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int r;
-	static int c;
-	static boolean[][] map;
-
+	static int r, c;
+	static char[][] graph;
+	// 오른쪽위 대각선, 오른쪽, 오른쪽 아래 대각선
+	static int[] dx = {-1, 0, 1};
+	static int[] dy = {1, 1, 1};
+	static boolean[][] visited;
+	static int answer = 0;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		r = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
-		map = new boolean[r][c];
+		
+		graph = new char[r][c];
+		visited = new boolean[r][c];
+		
 		for (int i = 0; i < r; i++) {
 			String str = br.readLine();
 			for (int j = 0; j < c; j++) {
-				if (str.charAt(j) == '.') map[i][j] = true;
+				graph[i][j] = str.charAt(j);
 			}
 		}
 		
-		int count = 0;
 		for (int i = 0; i < r; i++) {
-			if (isAble(i, 0)) count++;
+			if(dfs(i, 0) == true) {
+				answer ++;
+			}
 		}
-		System.out.println(count);
-	}
+		
+		System.out.println(answer);
 
-	private static boolean isAble(int i, int j) {
-		map[i][j] = false;
-		if (j == c - 1) return true;
-		for (int k = -1; k <= 1; k++) {
-			if (i + k < 0 || i + k >= r) continue;
-			if (!map[i + k][j + 1]) continue;
-			if (isAble(i + k, j + 1)) return true;
+	}
+	
+	private static boolean dfs(int x, int y) {
+		if(y == c - 1) {
+			return true;
 		}
+		
+		for (int d = 0; d < 3; d++) {
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			
+			if(0 <= nx && nx < r && 0 <= ny && ny < c) {
+				if(graph[nx][ny] != 'x' && !visited[nx][ny]) {
+					visited[nx][ny] = true;
+					if(dfs(nx, ny)) {
+						return true;
+					}
+				}
+			}
+		}
+		
 		return false;
+		
 	}
 
 }
