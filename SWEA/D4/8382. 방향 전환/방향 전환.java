8ce02a893c9172411b9248675cd9cbe68df8fa9a
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-17 13:51:30 +0900

    [D4] Title: 방향 전환, Time: 413 ms, Memory: 87,348 KB -BaekjoonHub

---
+++ SWEA/D4/8382. 방향 전환/방향 전환.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Solution {
+	
+	static int n = 100;
+	static int[][] dx = {{-1, 1}, {0, 0}};
+	static int[][] dy = {{0, 0}, {-1, 1}};
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int t = 1; t <= tc; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken()) + n;
+			int y1 = Integer.parseInt(st.nextToken()) + n;
+			int x2 = Integer.parseInt(st.nextToken()) + n;
+			int y2 = Integer.parseInt(st.nextToken()) + n;
+			sb.append("#" + t + " " + bfs(x1, y1, x2, y2) + "\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static int bfs(int x1, int y1, int x2, int y2) {
+		int[][][] visited = new int[2 * n + 1][2 * n + 1][2];
+		visited[x1][y1][0] = visited[x1][y1][1] = 1;
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] {x1, y1, 0});
+		queue.offer(new int[] {x1, y1, 1});
+		while (!queue.isEmpty()) {
+			int[] curr = queue.poll();
+			int x = curr[0];
+			int y = curr[1];
+			int k = curr[2];
+			for (int d = 0; d < 2; d++) {
+				int nx = x + dx[k][d];
+				int ny = y + dy[k][d];
+				if (!isIn(nx, ny)) continue;
+				if (visited[nx][ny][1 - k] > 0) continue;
+				visited[nx][ny][1 - k] = visited[x][y][k] + 1;
+				queue.offer(new int[] {nx, ny, 1 - k});
+			}
+		}
+		return Math.min(visited[x2][y2][0], visited[x2][y2][1]) - 1;
+	}
+
+	private static boolean isIn(int x, int y) {
+		return x >= 0 && x <= 2 * n && y >= 0 && y <= 2 * n;
+	}
+
+}
