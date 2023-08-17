c2c176c83ff719c2c56f3580d97adf010b3db0da
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-17 15:15:47 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 540 ms, Memory: 63716 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,67 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    int k;
-    int w, h;
-    int[][] board;
+	
+	static int k;
+	static int w;
+	static int h;
+	static int[][] map;
+	
+	static int[][] monkey = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+	static int[][] horse = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
 
-    void io() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        k = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        w = Integer.parseInt(st.nextToken());
-        h = Integer.parseInt(st.nextToken());
-        board = new int[h][w];
-        for (int i = 0; i < h; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < w; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        System.out.println(solution());
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		k = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		w = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+		map = new int[h][w];
+		for (int i = 0; i < h; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < w; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		System.out.println(bfs());
+	}
 
-    int solution() {
-        if (w == 1 && h == 1) return 0;
-        final int[][] HORSE = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
-        final int[][] DIR = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        Queue<int[]> queue = new LinkedList<>();
-        int[][][] dist = new int[h][w][k + 1];
-        queue.offer(new int[]{0, 0, 0}); // 좌표 2개, 말의 움직임 횟수
-        dist[0][0][0] = 1;
-        while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            for (int[] dir : DIR) {
-                int nextI = curr[0] + dir[0];
-                int nextJ = curr[1] + dir[1];
-                if (nextI < 0 || nextI >= h || nextJ < 0 || nextJ >= w) continue;
-                if (board[nextI][nextJ] == 1) continue;
-                if (dist[nextI][nextJ][curr[2]] > 0) continue;
-                dist[nextI][nextJ][curr[2]] = dist[curr[0]][curr[1]][curr[2]] + 1;
-                if (nextI == h - 1 && nextJ == w - 1) return dist[nextI][nextJ][curr[2]] - 1;
-                queue.offer(new int[]{nextI, nextJ, curr[2]});
-            }
-            if (curr[2] == k) continue;
-            for (int[] dir : HORSE) {
-                int nextI = curr[0] + dir[0];
-                int nextJ = curr[1] + dir[1];
-                if (nextI < 0 || nextI >= h || nextJ < 0 || nextJ >= w) continue;
-                if (board[nextI][nextJ] == 1) continue;
-                if (dist[nextI][nextJ][curr[2] + 1] > 0) continue;
-                dist[nextI][nextJ][curr[2] + 1] = dist[curr[0]][curr[1]][curr[2]] + 1;
-                if (nextI == h - 1 && nextJ == w - 1) return dist[nextI][nextJ][curr[2] + 1] - 1;
-                queue.offer(new int[]{nextI, nextJ, curr[2] + 1});
-            }
-        }
-        return -1;
-    }
+	private static int bfs() {
+		if (h == 1 && w == 1) return 0;
+		int[][][] visited = new int[h][w][k + 1];
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] {0, 0, 0});
+		visited[0][0][0] = 1;
+		while (!queue.isEmpty()) {
+			int[] curr = queue.poll();
+			int r = curr[0];
+			int c = curr[1];
+			int use = curr[2];
+			for (int d = 0; d < 4; d++) {
+				int nr = r + monkey[d][0];
+				int nc = c + monkey[d][1];
+				if (nr == h - 1 && nc == w - 1) return visited[r][c][use];
+				if (!isIn(nr, nc)) continue;
+				if (map[nr][nc] == 1) continue;
+				if (visited[nr][nc][use] > 0) continue;
+				visited[nr][nc][use] = visited[r][c][use] + 1;
+				queue.offer(new int[] {nr, nc, use});
+			}
+			if (use == k) continue;
+			for (int d = 0; d < 8; d++) {
+				int nr = r + horse[d][0];
+				int nc = c + horse[d][1];
+				if (nr == h - 1 && nc == w - 1) return visited[r][c][use];
+				if (!isIn(nr, nc)) continue;
+				if (map[nr][nc] == 1) continue;
+				if (visited[nr][nc][use + 1] > 0) continue;
+				visited[nr][nc][use + 1] = visited[r][c][use] + 1;
+				queue.offer(new int[] {nr, nc, use + 1});
+			}
+		}
+		return -1;
+	}
 
-    public static void main(String[] args) throws IOException {
-        new Main().io();
-    }
+	private static boolean isIn(int r, int c) {
+		return r >= 0 && r < h && c >= 0 && c < w;
+	}
+
 }
