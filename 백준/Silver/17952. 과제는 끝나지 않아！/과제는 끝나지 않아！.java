43085161030d30b3a312a7c699717d84311bfd7a
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-21 16:10:06 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 1140 ms, Memory: 236768 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.java
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.java
@@ -5,61 +5,37 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n;
-	static int work, score, minute;
-	static int now_score, now_time;
-	static int answer;
-	public static void main(String[] args) throws Exception, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		
-		// 스택 자료구조를 이용
-		Stack<int[]> stack = new Stack<>();
-		
-		// 정답을 위한 변수선언
-		answer = 0;
-		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			work = Integer.parseInt(st.nextToken());
-			
-			if(work == 1) {
-				score = Integer.parseInt(st.nextToken());
-				minute = Integer.parseInt(st.nextToken());
-				minute -= 1;
-				// 만약 바로 일처리가 가능하다면 점수를 획득
-				if (minute <= 0) {
-					answer += score;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 입력 스트림 열기
+		int n = Integer.parseInt(br.readLine()); // 분기가 총 몇분인지 n 입력 받기
+		int[][] work = new int[n][2]; // 일 정보 저장 : 0번째는 업무 점수, 1번째는 업무 처리하기 위해 남은 시간
+		Stack<Integer> stack = new Stack<>(); // 일하는 순서를 스택에 저장(무조건 새로운 업무부터 하기 때문)
+		int score = 0; // 업무 평가 점수 저장
+		for (int i = 0; i < n; i++) { // n분 동안
+			StringTokenizer st = new StringTokenizer(br.readLine()); // i번째 줄 입력을 공백 단위로 쪼개줌
+			int info = Integer.parseInt(st.nextToken()); // 첫 입력 : 0 또는 1
+			if (info == 0) { // 업무가 주어지지 않으면
+				if (stack.isEmpty()) continue; // 할 일이 없는 경우에는 아무것도 하지 않는다
+				int currWork = stack.peek(); // 할 일이 있다면 몇번째 일을 할지 찾는다
+				work[currWork][1]--; // 현재 하고 있는 일을 1분간 처리했으므로 남은 시간 1분 감소
+				if (work[currWork][1] == 0) { // 현재 하고 있는 일이 끝났다면
+					score += work[currWork][0]; // 업무 평가 점수에 현재 하고 있는 일에 대한 점수를 추가한다
+					stack.pop(); // 끝낸 일을 업무 스택에서 지운다
 				}
-				else {
-					// 남은 일처리를 스택에 넣어줌
-					stack.push(new int[] {score, minute});
-				}
-			}else {
-				// 큐가 비어있을 경우 예외처리
-				if(stack.isEmpty()) continue;
-				
-				// 0일경우 스택에서 꺼내서 일처리를 함
-				int[] now = stack.pop();
-				now_score = now[0];
-				now_time = now[1];
-				
-				// time을 1 감소시킴
-				now_time -= 1;
-				
-				// 만약 일처리가 끝났다면 스택에 넣지않고 점수만 계산
-				if(now_time <= 0) {
-					answer += now_score;
-					continue;
-				}
-				// 일처리가 남았다면 다시 스택에 넣어줌
-				else {
-					stack.push(new int[] {now_score, now_time});
-				}
-				
-			};
-			
-			
+				continue;
+			}
+			// 업무가 주어진다면
+			work[i][0] = Integer.parseInt(st.nextToken()); // 주어진 업무의 만점 입력 받기
+			work[i][1] = Integer.parseInt(st.nextToken()); // 주어진 업무의 소요 시간 입력 받기
+			work[i][1]--; // 최근 업무부터 처리하고, 1분간 처리했으므로 남은 시간 1분 감소
+			if (work[i][1] == 0) { // 처리하고 있는 업무가 끝났다면
+				score += work[i][0]; // 업무 평가 점수에 해당 업무에 대한 점수를 추가하고 끝낸다
+				continue;
+			}
+			stack.push(i); // 업무를 처리하고 있다면 업무 스택에 해당 업무를 추가한다
 		}
-		System.out.println(answer);
+		System.out.println(score); // 이번 분기 업무 평가 점수를 출력한다 
 	}
+
 }
