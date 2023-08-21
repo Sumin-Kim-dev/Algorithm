9d19776912129a8fb65ce215ce744f9a26ccce7b
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-21 13:11:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 15344 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,43 +1,49 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
-
-	static int n, k, time[];
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		n = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		time = new int[100001];
-		for (int i = 0; i < 100001; i++)
-			time[i] = -1;
-		bw.write(time() + "");
-		bw.close();
-	}
-
-	static int time() {
-		Queue<Integer> queue = new LinkedList<>();
-		time[n] = 0;
-		queue.add(n);
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		if (n == k) {
+			System.out.println(0);
+			return;
+		}
+		
+		int[] visited = new int[100001];
+		Queue<Integer> queue = new ArrayDeque<>();
+		visited[n] = 1;
+		queue.offer(n);
 		while (!queue.isEmpty()) {
-			int current = queue.poll();
-			if (current > 0 && time[current - 1] == -1) {
-				queue.add(current - 1);
-				time[current - 1] = time[current] + 1;
+			int curr = queue.poll();
+			if (2 * curr <= 100000 && visited[2 * curr] == 0) {
+				if (2 * curr == k) {
+					System.out.println(visited[curr]);
+					return;
+				}
+				queue.offer(2 * curr);
+				visited[2 * curr] = visited[curr] + 1;
 			}
-			if (current < 100000 && time[current + 1] == -1) {
-				queue.add(current + 1);
-				time[current + 1] = time[current] + 1;
+			if (curr - 1 >= 0 && visited[curr - 1] == 0) {
+				if (curr - 1 == k) {
+					System.out.println(visited[curr]);
+					return;
+				}
+				queue.offer(curr - 1);
+				visited[curr - 1] = visited[curr] + 1;
 			}
-			if (current * 2 <= 100000 && time[current * 2] == -1) {
-				queue.add(current * 2);
-				time[current * 2] = time[current] + 1;
+			if (curr + 1 <= 100000 && visited[curr + 1] == 0) {
+				if (curr + 1 == k) {
+					System.out.println(visited[curr]);
+					return;
+				}
+				queue.offer(curr + 1);
+				visited[curr + 1] = visited[curr] + 1;
 			}
 		}
-		return time[k];
 	}
+
 }
