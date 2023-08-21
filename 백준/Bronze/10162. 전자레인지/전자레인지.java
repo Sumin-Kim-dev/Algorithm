e7b48b21e8d8569e183f8b4271bd5284c24b0a57
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-21 16:09:00 +0900

    [Bronze III] Title: 전자레인지, Time: 112 ms, Memory: 12860 KB -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.java
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -1,60 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-	static int t;
-	static int a, b, c;
-	static int a_cnt, b_cnt, c_cnt;
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		// 요리시간 입력받기
-		t = sc.nextInt();
-		// 분을 초로 변경해서 저장
-		a = 300;
-		b = 60;
-		c = 10;
+		Scanner sc = new Scanner(System.in); // 입력 스트림 생성
+		int t = sc.nextInt(); // 시간 t 입력 받음
+		int[] button = {300, 60, 10}; // A버튼 5분 = 300초, B버튼 1분 = 60초, C버튼 10초
 		
-		// a, b, c 개수를 카운팅할 변수 선언
-		a_cnt = 0;
-		b_cnt = 0;
-		c_cnt = 0;
-		
-
-		while(true) {
-			// 10초 미만은 쿠킹불가
-			if(t < 10) {
-				break;
-			}
-			// 만약 5분이상 남았다면
-			if(t >= 500) {
-				t -= a;
-				a_cnt += 1;
-				continue;
-			// 1분 이상 남았다면
-			}else if(t >= 60) {
-				// 빼고
-				t -= 60;
-				// 카운팅
-				b_cnt += 1;
-				continue;
-			// 10초 이상 남았다면
-			}else if(t >= 10) {
-				// 빼고
-				t -= 10;
-				// 카운팅
-				c_cnt += 1;
-			}
-		}
-		// 모두 계산이 끝났는데 0초가 남았다면
-		if(t == 0) {
-			System.out.println(a_cnt + " " + b_cnt + " " + c_cnt);
-		}
-		// 잔여초가 있다면
-		else {
-			System.out.println(-1);
+		if (t % 10 != 0) { // t가 10의 배수가 아니면 불가능하다(최소 시간 단위가 10초)
+			System.out.println(-1); // 불가능하면 -1 출력
+			return; // 아래를 보지 않고 main을 끝낸다
 		}
 		
-
-		
+		// 300은 60의 배수이고 60은 10의 배수이므로 그리디 알고리즘 적용 가능
+		for (int i = 0; i < 3; i++) { // 각 버튼
+			System.out.print(t / button[i] + " "); // 그리디 : 시간 긴 버튼을 최대한 많이 써야 버튼 사용횟수가 최소, 현재 버튼 사용 횟수를 출력한다
+			t %= button[i]; // 남은 시간
+		}
 
 	}
 
