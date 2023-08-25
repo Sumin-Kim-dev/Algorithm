b3b0fa4c4ecaf78551015c5811afd74c05de6729
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-25 17:38:56 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 1,604 ms, Memory: 23,912 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -1,15 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
 	
+	static class Node {
+		int cnt;
+		int r;
+		int c;
+		Node next;
+		
+		public Node(int cnt, int r, int c, Node next) {
+			this.cnt = cnt;
+			this.r = r;
+			this.c = c;
+			this.next = next;
+		}
+	}
+	
 	static int n;
 	static int[][] map;
-	static List<int[]> cores;
+	static Node core;
 	static int nCore;
 	
 	static int maxConnected;
@@ -25,50 +37,49 @@ public class Solution {
 		for (int t = 1; t <= tc; t++) {
 			n = Integer.parseInt(br.readLine());
 			map = new int[n][n];
-			cores = new ArrayList<>();
+			core = null;
 			nCore = 0;
-			maxConnected = -1;
+			maxConnected = 0;
 			minLength = Integer.MAX_VALUE;
 			for (int i = 0; i < n; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < n; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
 					if(map[i][j] == 1) {
-						cores.add(new int[] {i, j});
-						nCore++;
+						core = new Node(nCore++, i, j ,core);
 					}
 				}
 			}
-			backtracking(0, 0, 0);
+			backtracking(core, 0, 0);
 			sb.append('#').append(t).append(' ').append(minLength).append('\n');
 		}
 		System.out.println(sb);
 	}
 
-	private static void backtracking(int cnt, int connected, int length) {
-		if (cnt == nCore) {
-			if (connected > maxConnected || connected == maxConnected && length < minLength) {
-				maxConnected = connected;
-				minLength = length;
-			}
+	private static void backtracking(Node core, int connected, int length) {
+		if (connected > maxConnected || connected == maxConnected && length < minLength) {
+			maxConnected = connected;
+			minLength = length;
+		}
+		if (core == null) {
 			return;
 		}
-		int[] core = cores.get(cnt);
 		// 연결 안함
-		backtracking(cnt + 1, connected, length);
+		backtracking(core.next, connected, length);
 		// 연결
+		int r = core.r;
+		int c = core.c;
+		int cnt = core.cnt;
 		for (int d = 0; d < 4; d++) {
-			if (canConnect(core, d)) {
-				connect(core, cnt, d);
-				backtracking(cnt + 1, connected + 1, length + dist(core, d));
-				disconnect(core, cnt, d);
+			if (canConnect(r, c, d)) {
+				connect(r, c, cnt, d);
+				backtracking(core.next, connected + 1, length + dist(r, c, d));
+				disconnect(r, c, cnt, d);
 			}
 		}
 	}
 
-	private static int dist(int[] core, int d) {
-		int r = core[0];
-		int c = core[1];
+	private static int dist(int r, int c, int d) {
 		if (d == 0) return r;
 		if (d == 1) return n - 1 - c;
 		if (d == 2) return n - 1 - r;
@@ -80,9 +91,9 @@ public class Solution {
 		return r >= 0 && r < n && c >= 0 && c < n;
 	}
 
-	private static boolean canConnect(int[] core, int d) {
-		int r = core[0] + dr[d];
-		int c = core[1] + dc[d];
+	private static boolean canConnect(int r, int c, int d) {
+		r += dr[d];
+		c += dc[d];
 		while (isIn(r, c)) {
 			if (map[r][c] > 0) return false;
 			r += dr[d];
@@ -91,9 +102,9 @@ public class Solution {
 		return true;
 	}
 	
-	private static void connect(int[] core, int cnt, int d) {
-		int r = core[0] + dr[d];
-		int c = core[1] + dc[d];
+	private static void connect(int r, int c, int cnt, int d) {
+		r += dr[d];
+		c += dc[d];
 		while (isIn(r, c)) {
 			map[r][c] = cnt + 2;
 			r += dr[d];
@@ -101,9 +112,9 @@ public class Solution {
 		}
 	}
 	
-	private static void disconnect(int[] core, int cnt, int d) {
-		int r = core[0] + dr[d];
-		int c = core[1] + dc[d];
+	private static void disconnect(int r, int c, int cnt, int d) {
+		r += dr[d];
+		c += dc[d];
 		while (isIn(r, c)) {
 			if (map[r][c] == cnt + 2) {
 				map[r][c] = 0;
