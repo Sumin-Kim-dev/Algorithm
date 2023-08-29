001140c4fa3da3c085a2f656779917f0458e1f26
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-29 14:22:17 +0900

    [Silver V] Title: 다리 놓기, Time: 80 ms, Memory: 11824 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,41 +1,35 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+	
+	static int[][] combi;
 
-	public static void main(String args[]) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        
-        int T = Integer.parseInt(br.readLine());
-        int N, M;
-        for(int i = 0; i < T; i++) {
-            st = new StringTokenizer(br.readLine());
-		    N = Integer.parseInt(st.nextToken());
-		    M = Integer.parseInt(st.nextToken());
-		    sb.append(Combination(M, N)+"\n");
-        }
-        bw.write(sb.toString());
-		bw.close();
-	}
-
-	static int Combination(int N, int K) {
-		int combination[] = new int[K + 1];
-		int temp[] = new int[K + 1];
-
-		// 0C0 = 1, 0C1, ..., 0Ck = 0
-		combination[0] = 1;
-
-		// nCk = (n-1)C(k-1)+(n-1)Ck
-		int i = 1;
-		while (i <= N) {
-			temp = combination.clone();
-			for (int j = 1; j < K + 1; j++)
-				combination[j] = (temp[j - 1] + temp[j]);
-			i++;
+		int t = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		setCombi();
+		while (t-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			sb.append(combi[m][n]).append('\n');
 		}
-		return combination[K];
+		System.out.println(sb);
 	}
+
+	private static void setCombi() {
+		combi = new int[30][30];
+		combi[0][0] = 1;
+		for (int i = 1; i < 30; i++) {
+			combi[i][0] = 1;
+			for (int j = 1; j < 30; j++) {
+				combi[i][j] = combi[i - 1][j - 1] + combi[i - 1][j];
+			}
+		}
+	}
+
 }
