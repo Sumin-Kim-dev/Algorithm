4c6e7afce3d8b5458f827ef5f39479cb3c9dd1a5
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-08-30 14:24:33 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 108 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -1,65 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    int n, m;
-    char[][] maze;
-    int[][][] minDist;
+	
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
 
-    public static void main(String[] args) throws IOException {
-        new Main().io();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		char[][] map = new char[n][m];
+		int sr = 0;
+		int sc = 0;
+		for (int i = 0; i < n; i++) {
+			map[i] = br.readLine().toCharArray();
+			for (int j = 0; j < m; j++) {
+				if (map[i][j] == '0') {
+					sr = i;
+					sc = j;
+				}
+			}
+		}
+		int[][][] visited = new int[n][m][1 << 6];
+		visited[sr][sc][0] = 1;
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] {sr, sc, 0});
+		while (!queue.isEmpty()) {
+			int[] curr = queue.poll();
+			int r = curr[0];
+			int c = curr[1];
+			int key = curr[2];
+			for (int d = 0; d < 4; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+				int nextKey = key;
+				if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+				if (map[nr][nc] == '#') continue;
+				if (map[nr][nc] >= 'A' && map[nr][nc] <= 'F') {
+					if ((key & (1 << (map[nr][nc] - 'A'))) == 0) continue;
+				}
+				if (map[nr][nc] >= 'a' && map[nr][nc] <= 'f') {
+					nextKey = key | (1 << (map[nr][nc] - 'a'));
+				}
+				if (map[nr][nc] == '1') {
+					System.out.println(visited[r][c][key]);
+					return;
+				}
+				if (visited[nr][nc][nextKey] > 0) continue;
+				visited[nr][nc][nextKey] = visited[r][c][key] + 1;
+				queue.offer(new int[] {nr, nc, nextKey});
+			}
+		}
+		System.out.println(-1);
+	}
 
-    void io() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        maze = new char[n][m];
-        int[] start = new int[3];
-        for (int i = 0; i < n; i++) {
-            maze[i] = br.readLine().toCharArray();
-            for (int j = 0; j < m; j++) {
-                if (maze[i][j] == '0') {
-                    start[0] = i;
-                    start[1] = j;
-                }
-            }
-        }
-        System.out.println(bfs(start));
-    }
-
-    static final int[][] DIR = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-    int bfs(int[] start) {
-        minDist = new int[n][m][1 << 6];
-        minDist[start[0]][start[1]][start[2]] = 1;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(start);
-        while(!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            for (int[] dir : DIR) {
-                int[] next = {curr[0] + dir[0], curr[1] + dir[1], curr[2]};
-                if (next[0] < 0 || next[0] >= n || next[1] < 0 || next[1] >= m) continue;
-                if (maze[next[0]][next[1]] == '#') continue;
-                if (maze[next[0]][next[1]] == '1') return minDist[curr[0]][curr[1]][curr[2]];
-                if (Character.isUpperCase(maze[next[0]][next[1]])) {
-                    int door = maze[next[0]][next[1]] - 'A';
-                    if ((next[2] & (1 << door)) == 0) continue;
-                }
-                if (Character.isLowerCase(maze[next[0]][next[1]])) {
-                    int key = maze[next[0]][next[1]] - 'A';
-                    next[2] |= (1 << key);
-                }
-                if (minDist[next[0]][next[1]][next[2]] > 0) continue;
-                minDist[next[0]][next[1]][next[2]] = minDist[curr[0]][curr[1]][curr[2]] + 1;
-                queue.offer(next);
-            }
-        }
-        return -1;
-    }
 }
