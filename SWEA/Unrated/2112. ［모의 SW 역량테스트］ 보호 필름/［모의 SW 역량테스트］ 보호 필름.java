0ce97e105dead9fcec0f4235fb3bef6ec652d733
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-09-25 14:05:19 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 609 ms, Memory: 108,620 KB -BaekjoonHub

---
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+	
+	static int d;
+	static int w;
+	static int k;
+	static int[][] map;
+	static int min;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int t = 1; t <= tc; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			d = Integer.parseInt(st.nextToken());
+			w = Integer.parseInt(st.nextToken());
+			k = Integer.parseInt(st.nextToken());
+			map = new int[d][w];
+			min = k;
+			for (int i = 0; i < d; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < w; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			backtracking(0, 0);
+			sb.append('#').append(t).append(' ').append(min).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+	private static void backtracking(int cnt, int start) {
+		if (isPass()) {
+			min = cnt;
+			return;
+		}
+		if (cnt >= min) return;
+		for (int i = start; i < d; i++) {
+			int[] before = map[i].clone();
+			Arrays.fill(map[i], 0);
+			backtracking(cnt + 1, i + 1);
+			Arrays.fill(map[i], 1);
+			backtracking(cnt + 1, i + 1);
+			map[i] = before;
+		}
+	}
+
+	private static boolean isPass() {
+		int[] sum = new int[w];
+		for (int i = 0; i < w; i++) {
+			for (int j = 0; j < k; j++) {
+				sum[i] += map[j][i];
+			}
+			for (int j = k; j < d; j++) {
+				if (sum[i] == 0 || sum[i] == k) break;
+				sum[i] += map[j][i];
+				sum[i] -= map[j - k][i];
+			}
+			if (sum[i] != 0 && sum[i] != k) return false;
+		}
+		return true;
+	}
+
+}
