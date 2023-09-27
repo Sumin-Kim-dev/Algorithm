15dc036f82e0afd807b000f80ee0bf2a451052b7
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-09-27 13:20:24 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 1,446 ms, Memory: 96,704 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -1,10 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -16,35 +12,25 @@ public class Solution {
 		for (int t = 1; t <= tc; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
-			List<Integer>[] adj = new List[n];
+			int[][] dist = new int[n][n];
 			for (int i = 0; i < n; i++) {
-				adj[i] = new ArrayList<>();
 				for (int j = 0; j < n; j++) {
-					if (Integer.parseInt(st.nextToken()) == 1) {
-						adj[i].add(j);
+					dist[i][j] = Integer.parseInt(st.nextToken());
+					if (i != j && dist[i][j] == 0) dist[i][j] = 10000;
+				}
+			}
+			for (int k = 0; k < n; k++) {
+				for (int i = 0; i < n; i++) {
+					for (int j = 0; j < n; j++) {
+						dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
 					}
 				}
 			}
 			int min = n * n;
 			for (int i = 0; i < n; i++) {
 				int cc = 0;
-				boolean[] isVisited = new boolean[n];
-				isVisited[i] = true;
-				Queue<Integer> queue = new ArrayDeque<>();
-				queue.offer(i);
-				int d = 0;
-				while (!queue.isEmpty()) {
-					int size = queue.size();
-					cc += d * size;
-					for (int j = 0; j < size; j++) {
-						int curr = queue.poll();
-						for (int next : adj[curr]) {
-							if (isVisited[next]) continue;
-							isVisited[next] = true;
-							queue.offer(next);
-						}
-					}
-					d++;
+				for (int j = 0; j < n; j++) {
+					cc += dist[i][j];
 				}
 				min = Math.min(min, cc);
 			}
