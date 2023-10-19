6da8051f8d2a0f57a44c88f4a4ea47c9c663e13d
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-10-19 10:11:21 +0900

    [Gold III] Title: 택배, Time: 284 ms, Memory: 18900 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.java
+++ 백준/Gold/1719. 택배/택배.java
@@ -1,44 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	static final int INF = 100_000_000;
+	static final int INF = 10_000_000;
 	
 	static int n;
-	static List<int[]>[] adj;
-	static int[] minDist;
+	static int[][] minDist;
 	static int[][] table;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
-		adj = new List[n];
-		for (int i = 0; i < n; i++) {
-			adj[i] = new ArrayList<>();
-		}
+		minDist = new int[n][n];
+		table = new int[n][n];
 		int m = Integer.parseInt(st.nextToken());
 		while (m-- > 0) {
 			st = new StringTokenizer(br.readLine());
 			int s = Integer.parseInt(st.nextToken()) - 1;
 			int e = Integer.parseInt(st.nextToken()) - 1;
 			int w = Integer.parseInt(st.nextToken());
-			adj[s].add(new int[] {e, w});
-			adj[e].add(new int[] {s, w});
+			minDist[s][e] = minDist[e][s] = w;
+			table[s][e] = e;
+			table[e][s] = s;
 		}
-		minDist = new int[n];
-		table = new int[n][n];
-		for (int i = 0; i < n; i++) {
-			dijkstra(i);
-		}
+		floydWarshall();
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
@@ -51,22 +40,21 @@ public class Main {
 		System.out.println(sb);
 	}
 
-	private static void dijkstra(int i) {
-		Arrays.fill(minDist, INF);
-		minDist[i] = 0;
-		PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
-		pq.offer(new int[] {i, 0});
-		while (!pq.isEmpty()) {
-			int[] curr = pq.poll();
-			int c = curr[0];
-			int d = curr[1];
-			if (minDist[c] < d) continue;
-			for (int[] e : adj[c]) {
-				if (minDist[e[0]] <= minDist[c] + e[1]) continue;
-				minDist[e[0]] = minDist[c] + e[1];
-				pq.offer(new int[] {e[0], minDist[e[0]]});
-				if (c == i) table[i][e[0]] = e[0];
-				else table[i][e[0]] = table[i][c];
+	private static void floydWarshall() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i == j || minDist[i][j] > 0) continue;
+				minDist[i][j] = INF;
+			}
+		}
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (minDist[i][j] > minDist[i][k] + minDist[k][j]) {
+						minDist[i][j] = minDist[i][k] + minDist[k][j];
+						table[i][j] = table[i][k];
+					}
+				}
 			}
 		}
 	}
