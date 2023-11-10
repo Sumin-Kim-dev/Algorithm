55f12aa2711bb4cfd820426bdc54e753dda22642
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-11-11 08:33:45 +0900

    [Bronze IV] Title: Winning Score, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/17009. Winning Score/Winning Score.py
@@ -0,0 +1,8 @@
+a = 3 * int(input()) + 2 * int(input()) + int(input())
+b = 3 * int(input()) + 2 * int(input()) + int(input())
+if a > b:
+    print('A')
+elif a < b:
+    print('B')
+else:
+    print('T')
