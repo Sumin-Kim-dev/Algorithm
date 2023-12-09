714b1ccbb4d72ff5bbdad385b4a4f8fa0d6da51e
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2023-12-10 03:47:22 +0900

    [Bronze IV] Title: Gnome Sequencing, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.py
@@ -0,0 +1,8 @@
+n = int(input())
+print('Gnomes:')
+for _ in range(n):
+    a, b, c = map(int, input().split())
+    if a > b and b > c or a < b and b < c:
+        print('Ordered')
+    else:
+        print('Unordered')
