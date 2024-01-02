88c1149dc18bdbb367fc447c731135eefde6d832
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2024-01-03 00:26:25 +0900

    [Bronze IV] Title: Every Second Counts, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/15080. Every Second Counts/Every Second Counts.py
@@ -0,0 +1,7 @@
+h1, m1, s1 = map(int, input().split(" : "))
+h2, m2, s2 = map(int, input().split(" : "))
+d = (h2 * 3600 + m2 * 60 + s2) - (h1 * 3600 + m1 * 60 + s1)
+if d > 0:
+    print(d)
+else:
+    print(d + 86400)
