e5c3383244fe7ba60b7209ae25ecef266b76b252
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2024-01-09 04:23:59 +0900

    [Bronze IV] Title: Archivist, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.py
@@ -0,0 +1,7 @@
+n = int(input())
+if n == 1996 or n == 1997 or n == 2000 or n == 2007 or n == 2008 or n == 2013 or n == 2018:
+    print('SPbSU')
+elif n == 2006:
+    print('PetrSU, ITMO')
+else:
+    print('ITMO')
